0386c451be28c7e1cd3a074290e3b52618453a7f
Yosef-ft <yosefetene02@gmail.com>
2024-10-12 13:02:49 +0300

    Added a funtion to resample the timeframe

--- scripts/Utils.py
+++ scripts/Utils.py
@@ -2,7 +2,10 @@ import os
 import sys
 import datetime
 import logging
+import pandas as pd
 
+TF_EQUIV = {'1m' : '1Min', '5m': '5Min', '15m': '15Min', '30m':'30Min', '1h': '1H',
+            '4h': '4H', '6h':'6H', '12h':'12H', '1d' : 'D'}
 
 class Util:
 
@@ -50,6 +53,13 @@ class Util:
         return datetime.datetime.fromtimestamp(ms / 1000)
 
 
+    def resample_timeframe(self, data: pd.DataFrame, tf: str)-> pd.DataFrame:
+        '''
+        This funtion is used to resample the data from dataframe 1 minutes to different timeframes
+        '''
+        return data.resample(TF_EQUIV[tf]).agg(
+                {'Open' : 'first', 'High' : 'max', 'Low' : 'min', 'Close':'last', 'Volume': 'sum'}
+            )
 
 
 
